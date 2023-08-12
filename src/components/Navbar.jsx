da7b3d5209ae68885e4bda5be25a548e4c619277
Sean Park <seansangpark@gmail.com>
2023-08-12 11:55:32 -0400

    responding to click using handleNav

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -3,18 +3,22 @@ import { HiMenuAlt3 } from 'react-icons/hi';
 
 const Navbar = () => {
   const [nav, setNav] = useState(false);
+  const handleNav = () => {
+    setNav(!nav);
+  };
 
   return (
     <div className='absolute w-full flex justify-between p-4 items-center'>
       <h1 className='text-white font-bold text-2xl z-20'>Experiences</h1>
       <HiMenuAlt3
+        onClick={handleNav}
         className='z-20 text-white cursor-pointer'
         size={25}
       />
       <div
         className={
           nav
-            ? 'fixed text-gray-300 left-0 top-0 w-full h-screen bg-black/90 px-4 py-7 flex-col z-10'
+            ? 'ease-in duration-300 fixed text-gray-300 left-0 top-0 w-full h-screen bg-black/90 px-4 py-7 flex-col z-10'
             : 'absolute top-0 h-screen left-[-100%] ease-in duration-500 z-10'
         }
       >
