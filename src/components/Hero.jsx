d065880ab6a25175fa76e724e5bb1c3ed29c451b
Sean Park <seansangpark@gmail.com>
2023-08-12 17:31:37 -0400

    hero text changes

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -12,20 +12,15 @@ const Hero = () => {
       <div className='bg-black/30 absolute top-0 left-0 w-full h-screen' />
       <div className='absolute top-0 w-full h-full flex flex-col justify-center text-white'>
         <div className='md:left-[10%] max-w-[1100px] m-auto absolute p-4'>
-          <p>All Inclusive</p>
+          <p>All inclusive</p>
           <h1 className='font-bold text-5xl md:text-7xl drop-shadow-2xl'>
-            All Your Landscaping Needs
+            Lawn & Landscaping Services
           </h1>
           <p className='max-w-[600px] drop-shadow-2xl py-2 text-xl'>
             Dolore quis magna commodo do consequat Lorem. Non quis aliquip
             occaecat aliquip elit eiusmod consequat proident in. Voluptate do
-            enim fugiat aute magna amet irure cillum dolor eiusmod. Dolor sint
-            enim velit excepteur in sunt incididunt labore. Consequat labore
-            ipsum ipsum tempor qui deserunt eu eu anim reprehenderit tempor.
-            Consectetur id ullamco excepteur occaecat occaecat occaecat
-            consectetur ex sit exercitation amet.
           </p>
-          <button className='bg-white text-black'>Reserve Now</button>
+          <button className='bg-white text-black py-3'>Contact Now</button>
         </div>
       </div>
     </div>
