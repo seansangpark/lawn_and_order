85870bdbc954f820dfc06382d1b4d52c001ee4ab
Sean Park <seansangpark@gmail.com>
2023-08-12 17:24:20 -0400

    footer complete

--- src/App.js
+++ src/App.js
@@ -4,6 +4,7 @@ import Offers from './components/Offers';
 import Plan from './components/Plan';
 import Rooms from './components/Rooms';
 import ImageSlider from './components/ImageSlider';
+import Footer from './components/Footer';
 
 const App = () => {
   return (
@@ -14,6 +15,7 @@ const App = () => {
       <Plan />
       <Rooms />
       <ImageSlider />
+      <Footer />
     </div>
   );
 };
