33a55ef79ad44734320cb50f82c8ed09c2728bc3
Sean Park <seansangpark@gmail.com>
2023-09-22 16:14:53 -0400

    line between offers

--- src/components/Offers.jsx
+++ src/components/Offers.jsx
@@ -4,8 +4,11 @@ const Offers = () => {
   return (
     <div className='max-w-[900px] m-auto px-4 py-12 flex flex-wrap justify-between'>
       <p className='text-xl font-bold text-gray-700'>Lawn Mowing</p>
+      <p className='text-xl font-bold text-gray-700'> | </p>
       <p className='text-xl font-bold text-gray-700'>Lawn Fertilizer</p>
+      <p className='text-xl font-bold text-gray-700'> | </p>
       <p className='text-xl font-bold text-gray-700'>Landscaping Maintenance</p>
+      <p className='text-xl font-bold text-gray-700'> | </p>
       <p className='text-xl font-bold text-gray-700'>
         Landscaping Installation
       </p>
